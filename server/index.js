338479e03e2862a13bc220a555cbef446f749aa2
Rogue <prettale4@gmail.com>
2023-03-21 17:28:11 -0600

    added get endpoint to server

--- server/index.js
+++ server/index.js
@@ -6,4 +6,8 @@ app.use(cors());
 
 app.use(express.json())
 
+const {getTasks} = require('./controller.js')
+
+app.get('/api/tasks', getTasks)
+
 app.listen(6789, () => console.log('Docked at port 6789.'))
