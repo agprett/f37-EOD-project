320379c9749fa9eb961b98789b3d17a55280c123
Rogue <prettale4@gmail.com>
2023-03-23 17:33:11 -0600

    finished post functionality and auto check completed tasks

--- server/controller.js
+++ server/controller.js
@@ -1,8 +1,8 @@
-let task1 = {name: 'Sweep the floor', priority: 'Low', status: false}
+let task1 = {name: 'Sweep the floor', priority: 'Low', status: true}
 
 let task2 = {name: 'Get groceries', priority: 'Medium', status: true}
 
-let task3 = {name: 'Make dinner', priority: 'High', status: false}
+let task3 = {name: 'Make dinner', priority: 'High', status: true}
 
 let tasks = [task1, task2, task3]
 
